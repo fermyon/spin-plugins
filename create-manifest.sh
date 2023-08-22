6f8f28b4dbc732b3fdb2478ad227e4d4b3b12ed0
Thorsten Hans <thorsten.hans@gmail.com>
2023-08-22 16:53:58 +0200

    fix: load example.json from the correct location
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- create-manifest.sh
+++ create-manifest.sh
@@ -7,7 +7,7 @@ NAME=${1:-temp}
 echo "Creating a new manifest for Spin plugin ${NAME}."
 
 PLUGIN_DIR="${SCRIPT_DIR}/manifests/${NAME}"
-EXAMPLE_PLUGIN="${SCRIPT_DIR}/manifests/example/example.json"
+EXAMPLE_PLUGIN="${SCRIPT_DIR}/example/example.json"
 mkdir $PLUGIN_DIR
 PLUGIN_MANIFEST="${PLUGIN_DIR}/${NAME}.json"
 echo "" > $PLUGIN_MANIFEST
